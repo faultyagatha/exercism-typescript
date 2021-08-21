274d691d32c4c2692b4c769aaa7cf47b443d4379
faultyagatha <valeria.barvinska@gmail.com>
2021-08-21 14:06:38 +0200

    add rna solution

---
+++ rna-transcription/rna-transcription.ts
@@ -0,0 +1,24 @@
+interface ITranscriptorMap {
+  [index: string]: string
+}
+const map: ITranscriptorMap = {
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U',
+};
+
+class Transcriptor {
+  toRna(dna: string): string {
+    let rna = '';
+    for (let i = 0; i < dna.length; i++) {
+      if (!map[dna[i]]) {
+        throw new Error('Invalid input DNA.');
+      }
+      rna += map[dna[i]];
+    }
+    return rna;
+  }
+}
+
+export default Transcriptor
