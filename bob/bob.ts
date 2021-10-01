67565018999f368a481bd927674f237ea2ac691d
faultyagatha <valeria.barvinska@gmail.com>
2021-10-01 08:39:44 +0200

    add bob solution

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,3 +1,28 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+/** helpers */
+const isQuestion = (message: string): boolean => message.slice(-1) === '?';
+
+const isUpper = (message: string): boolean => message === message.toUpperCase();
+
+const isSilence = (message: string): boolean => message === " ";
+
+const hasLetters = /[^\W\d_]+/g;
+
+export function hey(message: string): string {
+  //preprocess the string
+  message = message.trim();
+
+
+  switch (true) {
+    case isSilence(message):
+      return "Fine. Be that way!";
+    case isUpper(message) && !isQuestion(message):
+      return "Whoa, chill out!";
+    case isQuestion(message) && !isUpper(message):
+      return 'Sure.';
+    case isUpper(message) && isQuestion(message):
+      return "Calm down, I know what I'm doing!";
+    default:
+      return "Whatever."
+  }
 }
+
